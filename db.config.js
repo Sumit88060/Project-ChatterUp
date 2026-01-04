91b165c71ef9ea8e3f5fc0a830803f14a576721c
Sumit88060 <134691040+Sumit88060@users.noreply.github.com>
2026-01-04 15:49:51 +0530

    Update db.config.js

--- db.config.js
+++ db.config.js
@@ -1,15 +1,14 @@
 import mongoose from "mongoose";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 dotenv.config();
 
-export const connectData = async()=>{
-
-    try{
-   await mongoose.connect(process.env.MONGO_URL);
-   console.log('succefully connect to database');
-    }catch(err){
-        console.log("failed to connect",err);
-    }
-
-}
+export const connectData = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Successfully connected to database");
+  } catch (err) {
+    console.error("Failed to connect to database", err);
+    process.exit(1); // stop app if DB fails
+  }
+};
